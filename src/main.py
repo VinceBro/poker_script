85cc748940369e5873b11c10b6e063450beb32df
VinceBro <43644883+VinceBro@users.noreply.github.com>
2019-02-11 15:54:05 -0500

    initialize dans main.py regarde valid du apikey/si un user existe deja

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import json,requests, pickle, datetime, sys
+import json,requests, pickle, datetime, sys, time
 #from script import *
 
 def initialize():
@@ -9,25 +9,67 @@ def initialize():
         with open(username + '.pickle', "rb") as fich:
             odds = pickle.loads(fich.read())
         print("Welcome back " + username + " !")
+        odds.initialize()
+        return odds
+
     except FileNotFoundError or KeyError:
         print("No such username was found")
-        apikey = ' '
-        valide = False
-        apikey = input("Enter valid API key: ")
+        time.sleep(.500)
+        print("Creating new user")
+        time.sleep(.500)
+        apikey = " "
+        response = {}
+        URL = "https://sf-api-on-demand-poker-odds-v1.p.mashape.com/flop?board=As%2C2h%2CTh&hole=Ac%2C3c"
 
+
+        while True:
+
+            apikey = input("Please enter a valid API key: ")
+            headers={
+              "X-Mashape-Key": apikey,
+              "Accept": "application/json"
+            }
+
+                try:
+                    response = requests.get(url=URL, headers=headers)  # type: object
+                    response = json.loads(response.text)
+                    response['message']
+                    continue
+
+                except KeyError:
+                    print("Valid API key")
+                    time.sleep(.500)
+                    print("New user created: " + username)
+                    time.sleep(.500)
+                    odds = odds(username, apikey)
+                    odds.initialize()
+                    return odds
 
 if __name__ == "__main__":
     # odds = odds()
+
     response = {}
     URL = "https://sf-api-on-demand-poker-odds-v1.p.mashape.com/flop?board=As%2C2h%2CTh&hole=Ac%2C3c"
+    apikey = input("Enter apikey: \n")
     headers={
-      "X-Mashape-Key": "CwTYMXYWmRmshP8DG1HkXmRgYqySp1298F2jsnVzKB3GGN0cKM",
+      "X-Mashape-Key": apikey,
       "Accept": "application/json"
     }
-    response = requests.get(url=URL, headers=headers)  # type: object
-    print(response)
-    response = json.loads(response.text)
-    print(response)
+    try:
+        response = requests.get(url=URL, headers=headers)  # type: object
+        print(response)
+        response = json.loads(response.text)
+        print(response)
+        response['message']
+        print("were not in boys")
+
+    except KeyError:
+        print("were in boys!!")
+    # try response['message']:
+    #     print("were not in boys")
+    # except KeyError:
+    #     print("were in boys")
+
 
     # response = unirest.get("https://sf-api-on-demand-poker-odds-v1.p.mashape.com/flop?board=As%2C2h%2CTh&hole=Ac%2C3c",
     #               headers={
--- src/script.py
+++ src/script.py
@@ -36,7 +36,11 @@ class odds():
     def __init__(self, username, apikey):
         self.counter = 0
         self.apikey = apikey
-        self.url = "TODO : determine url"
+        self.headers = {
+              "X-Mashape-Key": apikey,
+              "Accept": "application/json"
+            }
+        self.url = " "
         self.username = username
         self.NAME = " "
         self.hand = []
@@ -45,6 +49,7 @@ class odds():
         self.continuer = "y"
         self.day = datetime.date.today()
         self.shutdown = ""
+        self.response = {}
 
     def initialize(self):
         if self.day != datetime.date.today():
@@ -59,28 +64,21 @@ class odds():
         return None
 
     # pf == pre-flop,
-    def request(self, turn, board, hole):
+    def request(self, turn, board = [], hole):
         self.counter += 1
         if self.counter >= 95 and self.day == datetime.date.today():
             self.store()
             sys.exit("Daily limit reached try again tomorrow")
 
         if turn == "pre-flop":
-
-            response = unirest.get("https://sf-api-on-demand-poker-odds-v1.p.mashape.com/pre-flop?hole=" + convert_to_url(hole),
-              headers={
-                "X-Mashape-Key": "CwTYMXYWmRmshP8DG1HkXmRgYqySp1298F2jsnVzKB3GGN0cKM",
-                "Accept": "application/json"
-              }
-            )
-
+            self.url = "https://sf-api-on-demand-poker-odds-v1.p.mashape.com/pre-flop?hole=" + convert_to_url(hole)
         else:
-            response = unirest.get("https://sf-api-on-demand-poker-odds-v1.p.mashape.com/" + turn + "?board=" + convert_to_url(board) + "&hole=" + convert_to_url(hole) ,
-              headers={
-                "X-Mashape-Key": "CwTYMXYWmRmshP8DG1HkXmRgYqySp1298F2jsnVzKB3GGN0cKM",
-                "Accept": "application/json"
-              }
-            )
+            self.url = "https://sf-api-on-demand-poker-odds-v1.p.mashape.com/" + turn + "?board=" + convert_to_url(board) + "&hole=" + convert_to_url(hole)
+
+        self.response = requests.get(url=self.url, headers=self.headers)
+        self.response = json.loads(response.text)
+        #TODO faut décider ici si on return le response sans l'enregistrer dans la classe ou on return rien et on l'enregistre dans la classe
+
     def count(self):
         self.counter += 1
         if self.counter == 95:
@@ -106,6 +104,7 @@ class odds():
         print("Your initial odds of winning this hand (pre-flop) are: " + str(self.odds))
         while True:
             self.continuer = input("Do you wish to continue (y or n)?")
+            self.n_shutdown(self.continuer)
             if (self.continuer).lower() == "y":
                 for i in range(3):
                     self.continuer = input("Enter the first 3 community cards (" + str(i+ 1) + "/3) -- enter n to stop: ")
@@ -115,7 +114,6 @@ class odds():
 
                 print("the flop cards for this turn are  " + str(self.community))
                 break
-            self.n_shutdown(self.continuer)
 
     ## TODO: strip si jamais le chum entre des espaces!!!!!!!!!
     ## TODO ajout des 52 cartes pour vérifier l'input du user
