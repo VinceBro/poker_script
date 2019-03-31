8122acbfec915a10d1e75388c0e93dbc956c8cc7
VinceBro <vbreault99@gmail.com>
2019-03-30 22:35:46 -0400

    ca sauce pas mal

--- src/cython/handpotential.pyx
+++ src/cython/handpotential.pyx
@@ -1,37 +1,43 @@
+from algorithm import *
 
-def HandPotential(current):
+
+def HandPotential(hand, community):
     """calcul de 178,365 probabilitées après le flop pour prédire le nombre de
     mains qui gagneront/perderont de la valeur après le turn et le river"""
     # HP_total = np.zeros((1,3))
     # HP = np.zeros((3,3))
+    odds = Odds()
     cdef int ahead, behind, tied, i, j, x, length
+    cdef float Ppot, Npot
     cdef int HP[3][3]
     cdef int HP_total[1][3]
 
-    # self.calculate()
-    # prob_array_calculate[0,0] = self.ahead
-    # prob_array_calculate[1,1] = self.tied
-    # prob_array_calculate[2,2] = self.behind
-    self.update_deck()
-    self.played = self.hand + self.community
+    # odds.calculate()
+    # prob_array_calculate[0,0] = odds.ahead
+    # prob_array_calculate[1,1] = odds.tied
+    # prob_array_calculate[2,2] = odds.behind
+    odds.hand = hand
+    odds.community = community
+    odds.update_deck()
+    odds.played = hand + community
     ahead = 0
-    behind = 2
     tied = 1
-    length = len(self.current_deck)
+    behind = 2
+    length = len(odds.current_deck)
     # print(length)
-    # print(len(self.all_4_cards))
+    # print(len(odds.all_4_cards))
     print("we're in")
     for i in range(length):
         for j in range(i + 1, length):
             # print(i)
-            self.cunter += 1
+            odds.cunter += 1
             bonheur = []
-            bonheur.append(self.current_deck[i])
-            bonheur.append(self.current_deck[j])
+            bonheur.append(odds.current_deck[i])
+            bonheur.append(odds.current_deck[j])
             joie = bonheur
-            bonheur += self.community
+            bonheur += odds.community
             ### bonheur est devenu
-            output = self.compare(self.played, bonheur)
+            output = odds.compare(odds.played, bonheur)
 
             ## win = 0, loss = 2, tie = 1
             if output[0] == "Win":
@@ -45,9 +51,9 @@ def HandPotential(current):
             # print(HP_total)
 
             ### POUR FLOP
-            for x in self.create_all_4_cards(joie):
-                self.cunter += 1
-                adjusted_rank = self.compare(self.played + x, bonheur + x)
+            for x in odds.create_all_4_cards(joie):
+                odds.cunter += 1
+                adjusted_rank = odds.compare(odds.played + x, bonheur + x)
                 if str(adjusted_rank[0]) == "Loss":
                     HP[index,ahead] += 1
                 elif str(adjusted_rank[0]) == "Tie" and index == 2:
@@ -57,5 +63,5 @@ def HandPotential(current):
 
     Ppot = (HP[behind, ahead]+(HP[behind, tied])/2 + (HP[tied,ahead])/2)/(HP_total[0, behind]+HP_total[0, tied])
     Npot = (HP[ahead,behind]+HP[tied,behind]/2 + HP[ahead,tied]/2)/(HP_total[0, ahead]+HP_total[0, tied])
-    print(self.cunter)
+    print(odds.cunter)
     return (Ppot, Npot)
